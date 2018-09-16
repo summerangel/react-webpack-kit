973d56b9dc1d2bdad49b9208503f39b10aceb46e
summer <lisi@ehousechina.com>
2018-09-17 00:57:30 +0800

    runtime fix

--- babel.config.js
+++ babel.config.js
@@ -18,6 +18,7 @@ module.exports = function (api) {
             "@babel/preset-react"
         ],
         plugins: [
+            '@babel/plugin-transform-runtime',
             [
                 '@babel/plugin-proposal-decorators',
                 {
